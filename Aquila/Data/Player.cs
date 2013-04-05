8e0a52cf32aee672d08cad44218d49b0f1236130
Wouter Groen <nomenmihiest@hotmail.com>
2013-04-05 21:42:48 +0200

    updated Player

--- Aquila/Data/Player.cs
+++ Aquila/Data/Player.cs
@@ -9,12 +9,53 @@ namespace Aquila.Data
 {
     class Player
     {
-        public Color color { get; set; }
-        public int spaceShipDevelopment { get; set; }
-        public int technologyDevelopment { get; set; }
+        public Color Color { get; set; }
+        public int SpaceShipDevelopment { get; set; }
+        public int TechnologyDevelopment { get; set; }
 
-        public int transportCards { get; set; }
-        public List<T> 
+        public int TransportCards { get; set; }
+        public List<PlanetCard> Hand { get; set; }
 
+        public int PlayerTradeableCards
+        {
+            get
+            {
+                return TechnologyDevelopment > 0 ? 4 : 3;
+            }
+        }
+
+        public int DeckTradeableCards
+        {
+            get
+            {
+                return TechnologyDevelopment > 1 ? 3 : 2;
+            }
+        }
+
+        public bool roundDown
+        {
+            get
+            {
+                return TechnologyDevelopment > 2;
+            }
+        }
+
+        public int maxCards
+        {
+            get
+            {
+                switch (SpaceShipDevelopment)
+                {
+                    case 1:
+                        return 10;
+                    case 2:
+                        return 11;
+                    case 3:
+                        return 13;
+                    default:
+                        return 9;
+                }
+            }
+        }
     }
 }
