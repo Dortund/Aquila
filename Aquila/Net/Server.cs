f9340ab6343445c6ceac71a4ee99c37acd6bb414
Max Maton <thexa4@gmail.com>
2013-04-05 22:24:47 +0200

    Started on the server

---
+++ Aquila/Net/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Aquila.Net
+{
+    class Server
+    {
+        public TcpListener Server { get; protected set; }
+        public bool IsRunning { get; set; }
+
+        public Server()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if(IsRunning)
+                    return;
+                IsRunning = true;
+
+                Server = new TcpListener(IPAddress.Any, 35353);
+                Server.Start();
+
+                while (IsRunning)
+                {
+                    
+                }
+                Server.Stop();
+            }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
