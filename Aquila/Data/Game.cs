71112b602725fc1257f825cfa5cb2502f1fb6cfd
Max Maton <thexa4@gmail.com>
2013-04-05 21:37:58 +0200

    Added game state class

---
+++ Aquila/Data/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquila.Data
+{
+    /// <summary>
+    /// Represents a running game
+    /// </summary>
+    class Game
+    {
+        public List<Player> Players { get; set; }
+        public GameState State { get; set; }
+        public Dictionary<GameState, Func<GameState, string, object>> Transitions { get; protected set; }
+
+        public enum GameState
+        {
+            Lobby = 0,
+        }
+    }
+}
